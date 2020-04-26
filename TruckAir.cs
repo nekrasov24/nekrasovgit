79b4dfc8f1d6135fe4892e758565b2ad4d6872c3
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-04-26 23:56:26 +0300

    Create Passenger.cs

---
+++ TruckAir.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace airpers
+{
+    public class TruckAir : Planes
+    {
+        public TruckAir(string name, int capacity, int carrying, int rangeflight, int fuel)
+            : base(name, capacity, carrying, rangeflight, fuel)
+        {
+            
+        }
+    }
+}
