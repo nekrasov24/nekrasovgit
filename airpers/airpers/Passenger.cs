94237af49dd34a8e8ca5f2385eff9ada6e0e244d
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-04-27 13:13:31 +0300

    Create ifelse.sln

---
+++ airpers/airpers/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace airpers
+{
+    public class Passenger : Planes
+    {
+        public Passenger(string name, int capacity, int carrying, int rangeflight, int fuel)
+            : base (name, capacity, carrying, rangeflight, fuel)
+        {
+            
+
+        }
+    }
+}
---
+++ airpers/airpers/pln.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace airpers
+{
+    public class Planes
+    {
+        public string Name { get; set; } // имя
+
+        public int Capacity { get; set; } // вместимость
+
+        public int Carrying { get; set; } // грузоподъемность
+
+        public int RangeFlight { get; set; } // дальность полета
+
+        public int Fuel { get; set; } // горючее
+
+       
+        public Planes (string name, int capacity, int carrying, int rangeflight, int fuel)
+        {
+            Name = name;
+
+            Capacity = capacity;
+
+            Carrying = carrying;
+
+            RangeFlight = rangeflight;
+
+            Fuel = fuel;
+        }
+
+        
+
+
+
+    }
+}
