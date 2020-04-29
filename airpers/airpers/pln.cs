6d7aaee01cd130a5402ad9b804a2ef8bdeaa419e
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-04-29 04:00:33 +0300

    Create pln.cs

---
+++ airpers/airpers/pln.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+
+namespace airpers
+{
+    public abstract class Planes 
+    {
+        public string Name { get; set; } // имя
+
+        public int Capacity { get; set; } // вместимость
+
+        public int RangeFlight { get; set; } // дальность полета
+
+        public int Fuel { get; set; } // горючее
+
+        
+
+
+        public Planes (string name, int capacity, int rangeflight, int fuel)
+        {
+            Name = name;
+
+            Capacity = capacity;
+
+            RangeFlight = rangeflight;
+
+            Fuel = fuel;
+        }
+
+
+
+        
+
+
+
+    }
+}
