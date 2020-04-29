1cf132cd39890bd0e894538736ee5b8451d5ceb2
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-04-29 04:00:27 +0300

    Create TruckAir.cs

---
+++ airpers/airpers/TruckAir.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace airpers
+{
+    public class TruckAir : Planes
+    {
+        public int Carrying { get; set; } // грузоподъемность
+
+        public TruckAir(string name, int capacity, int carrying, int rangeflight, int fuel)
+            : base(name, capacity, rangeflight, fuel)
+        {
+            Carrying = carrying;
+
+
+        }
+    }
+}
