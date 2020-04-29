c8e61ab08a14a71240c1bd27bd7816739c255566
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-04-29 04:00:37 +0300

    Create AirCompany.cs

---
+++ airpers/airpers/AirCompany.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Collections;
+
+namespace airpers
+{
+     public class AirCompany : TruckAir 
+    {
+        public string CompanyName;
+
+        public AirCompany(string name, string companyName, int capacity, int carrying, int rangeflight, 
+            int fuel, int sumcapacity, int sumarrying)
+             : base(name, capacity, carrying, rangeflight, fuel)
+        {
+
+            CompanyName = companyName;
+
+        }
+
+        
+
+
+        
+
+
+       
+    }
+}
