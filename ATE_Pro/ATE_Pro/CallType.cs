fe334860fa3d148b6fe0770e2ea19ea7b4e696ae
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:13 +0300

    Create CallType.cs

---
+++ ATE_Pro/ATE_Pro/CallType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillingSystem.Enums
+{
+
+    public enum CallType
+    {
+        IncomingCall,
+        OutgoingCall
+    }
+}
