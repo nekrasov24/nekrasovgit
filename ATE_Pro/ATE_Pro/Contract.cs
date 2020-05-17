8f9a0b35a9a5b72cb7ffebaab8e219df9619c218
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:16 +0300

    Create Contract.cs

---
+++ ATE_Pro/ATE_Pro/Contract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BillingSystem.Enums;
+using BillingSystem.Models.Interfaces;
+
+namespace BillingSystem.Models.Classes
+{
+    public class Contract : IContract
+    {
+        static Random random = new Random();
+
+        public Subscriber Subscriber { get; }
+        public int Number { get; }
+        public Tariff Tariff { get; }
+
+        public Contract(Subscriber subscriber, TariffType tariffType)
+        {
+            Subscriber = subscriber;
+            Number = random.Next(1000000, 9999999);
+            Tariff = new Tariff(tariffType);
+        }
+
+    }
+}
