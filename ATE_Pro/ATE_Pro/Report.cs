3497432d770ddd85a1ffcaaf7befba4092bc1625
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:55 +0300

    Create Report.cs

---
+++ ATE_Pro/ATE_Pro/Report.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BillingSystem.Models.Classes
+{
+    public class Report
+    {
+        private IList<ReportRecord> _listRecords;
+
+        public Report()
+        {
+            _listRecords = new List<ReportRecord>();
+        }
+
+        public void AddRecord(ReportRecord record)
+        {
+            _listRecords.Add(record);
+        }
+
+        public IList<ReportRecord> GetRecords()
+        {
+            return _listRecords;
+        }
+    }
+}
