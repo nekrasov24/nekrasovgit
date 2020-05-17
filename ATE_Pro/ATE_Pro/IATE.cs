a97e95700b1768d489e7a8bb26eedea2a10319c2
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:23 +0300

    Create IATE.cs

---
+++ ATE_Pro/ATE_Pro/IATE.cs
@@ -0,0 +1,16 @@
+﻿using ATE.Events;
+using ATE.Models.Classes;
+using BillingSystem.Enums;
+using BillingSystem.Models.Classes;
+using BillingSystem.Models.Interfaces;
+using Tariff = BillingSystem.Models.Classes.Tariff;
+
+namespace ATE.Models.Interfaces
+{
+    public interface IATE : IInfo<CallInfo>
+    {
+        Terminal GetNewTerminal(IContract contract);
+        IContract RegisterContract(Subscriber subscriber, TariffType type);
+        void CallingTo(object sender, ICallingEvent e);
+    }
+}
