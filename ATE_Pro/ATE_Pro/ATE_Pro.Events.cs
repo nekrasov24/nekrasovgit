42b38dc0b088bae6aad527c29ff5046709f5dabb
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:38:56 +0300

    Create ATE_Pro.Events.cs

---
+++ ATE_Pro/ATE_Pro/ATE_Pro.Events.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ATE.Enums;
+
+namespace ATE.Events
+{
+
+        public class AnswerEvent : EventArgs, ICallingEvent
+        {
+            public Guid Id { get; }
+            public int TelephoneNumber { get; }
+            public int CalledTelephoneNumber { get; }
+            public CallState StateInCall { get; }
+
+            public AnswerEvent(int number, int target, CallState state)
+            {
+                this.TelephoneNumber = number;
+                this.CalledTelephoneNumber = target;
+                this.StateInCall = state;
+            }
+
+            public AnswerEvent(Guid id, int number, int target, CallState state)
+            {
+                this.Id = id;
+                this.TelephoneNumber = number;
+                this.CalledTelephoneNumber = target;
+                this.StateInCall = state;
+            }
+        }
+
+}
