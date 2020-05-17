e364b3d67f1f1a26f9939a25a1b20bad9ee0fa59
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:42 +0300

    Create IReportRender.cs

---
+++ ATE_Pro/ATE_Pro/IReportRender.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BillingSystem.Enums;
+using BillingSystem.Models.Classes;
+
+namespace BillingSystem.Models.Interfaces
+{
+    public interface IReportRender
+    {
+        void Render(Report report);
+        IEnumerable<ReportRecord> SortCalls(Report report, TypeSort sortType);
+    }
+}
