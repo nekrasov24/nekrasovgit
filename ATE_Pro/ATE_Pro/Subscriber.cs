54b11511b898e35e052521b673c370b41a0e7b76
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:40:06 +0300

    Create Subscriber.cs

---
+++ ATE_Pro/ATE_Pro/Subscriber.cs
@@ -0,0 +1,26 @@
+﻿namespace BillingSystem.Models.Classes
+{
+    public class Subscriber
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Money { get; private set; }
+
+        public Subscriber(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Money = 30;
+        }
+
+        public void AddMoney(int money)
+        {
+            Money += money;
+        }
+
+        public void RemoveMoney(int money)
+        {
+            Money -= money;
+        }
+    }
+}
