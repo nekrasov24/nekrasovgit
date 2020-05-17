2429f6c40495680e0655cac6d8c3b5a9ec973dcf
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:06 +0300

    Create CallInfo.cs

---
+++ ATE_Pro/ATE_Pro/CallInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BillingSystem.Models.Classes
+{
+    public class CallInfo
+    {
+        public Guid Id { get; set; }
+        public int MyNumber { get; set; }
+        public int CalledNumber { get; set; }
+        public DateTime BeginCall { get; set; }
+        public DateTime EndCall { get; set; }
+        public int Cost { get; set; }
+
+        public CallInfo(int myNumber, int targetNumber, DateTime beginCall)
+        {
+            Id = Guid.NewGuid();
+            MyNumber = myNumber;
+            CalledNumber = targetNumber;
+            BeginCall = beginCall;
+        }
+    }
+}
