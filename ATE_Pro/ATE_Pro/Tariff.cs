e93bdb2858ff3e1769bdf3d9eae5b66d7f4bb26d
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:40:09 +0300

    Create Tariff.cs

---
+++ ATE_Pro/ATE_Pro/Tariff.cs
@@ -0,0 +1,35 @@
+﻿using BillingSystem.Enums;
+
+namespace BillingSystem.Models.Classes
+{
+    public class Tariff
+    {
+        public int CostOfMonth { get; }
+        public int CostOfCallPerMinute { get; }
+        public int LimitCallInMonth { get; }
+        public TariffType TariffType { get; }
+
+        public Tariff(TariffType type)
+        {
+            TariffType = type;
+
+            switch (TariffType)
+            {
+                case TariffType.Pro:
+                    {
+                        CostOfMonth = 30;
+                        LimitCallInMonth = 12;
+                        CostOfCallPerMinute = 1;
+                        break;
+                    }
+                default:
+                    {
+                        CostOfMonth = 0;
+                        LimitCallInMonth = 0;
+                        CostOfCallPerMinute = 0;
+                        break;
+                    }
+            }
+        }
+    }
+}
