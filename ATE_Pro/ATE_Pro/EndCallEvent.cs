3c5a78ac9ec9ebc4c105c23980a9d532890cdf0b
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:19 +0300

    Create EndCallEvent.cs

---
+++ ATE_Pro/ATE_Pro/EndCallEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATE.Events
+{
+    public class EndCallEvent : EventArgs, ICallingEvent
+    {
+        public Guid Id { get; }
+        public int TelephoneNumber { get; }
+        public int CalledTelephoneNumber { get; }
+
+        public EndCallEvent(Guid id, int number)
+        {
+            this.Id = id;
+            this.TelephoneNumber = number;
+        }
+    }
+}
