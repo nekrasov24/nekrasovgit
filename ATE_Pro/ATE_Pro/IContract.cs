e9991c35d71dd7aa1e370e79674aa99ffbdafb0f
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:33 +0300

    Create IContract.cs

---
+++ ATE_Pro/ATE_Pro/IContract.cs
@@ -0,0 +1,11 @@
+﻿using BillingSystem.Models.Classes;
+
+namespace BillingSystem.Models.Interfaces
+{
+    public interface IContract
+    {
+        Subscriber Subscriber { get; }
+        int Number { get; }
+        Tariff Tariff { get; }
+    }
+}
