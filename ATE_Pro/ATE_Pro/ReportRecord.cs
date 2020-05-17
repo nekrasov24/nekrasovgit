75c80f61631667806bfcf553b6251a48b7fd9006
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:58 +0300

    Create ReportRecord.cs

---
+++ ATE_Pro/ATE_Pro/ReportRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BillingSystem.Enums;
+
+namespace BillingSystem.Models.Classes
+{
+    public class ReportRecord
+    {
+        public CallType CallType { get; }
+        public int Number { get; }
+        public DateTime Date { get; }
+        public DateTime Time { get; }
+        public int Cost { get; }
+
+        public ReportRecord(CallType callType, int number, DateTime date, DateTime time, int cost)
+        {
+            CallType = callType;
+            Number = number;
+            Date = date;
+            Time = time;
+            Cost = cost;
+        }
+    }
+}
