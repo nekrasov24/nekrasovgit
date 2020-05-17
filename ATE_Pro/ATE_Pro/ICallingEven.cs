4404fd666b9d1ddc60fc8f46e7c07041e50639fa
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:29 +0300

    Create ICallingEven.cs

---
+++ ATE_Pro/ATE_Pro/ICallingEven.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATE.Events
+{
+    public interface ICallingEvent
+    {
+        Guid Id { get; }
+        int TelephoneNumber { get; }
+        int CalledTelephoneNumber { get; }
+
+    }
+}
