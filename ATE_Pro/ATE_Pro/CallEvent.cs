ebfbeacaf3a3a57da0f31fe270f37a28b4a3ee87
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-05-18 02:39:03 +0300

    Create CallEvent.cs

---
+++ ATE_Pro/ATE_Pro/CallEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATE.Events
+{
+    public class CallEvent : EventArgs, ICallingEvent
+    {
+        public Guid Id { get; }
+        public int TelephoneNumber { get; }
+        public int CalledTelephoneNumber { get; }
+
+        public CallEvent(int number, int target)
+        {
+            this.TelephoneNumber = number;
+            this.CalledTelephoneNumber = target;
+        }
+
+        public CallEvent(Guid id, int number, int target)
+        {
+            this.Id = id;
+            this.TelephoneNumber = number;
+            this.CalledTelephoneNumber = target;
+
+        }
+    }
+}
